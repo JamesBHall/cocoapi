9e907ce6f934ac62311e03220980bf1eb859b0ea
Jimmy Hall <jamhall@microsoft.com>
2019-09-26 15:30:47 -0400

    Added dependencies to setup.py

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,6 +1,7 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
+#from distutils.core import setup
+from setuptools import setup, Extension
+#from Cython.Build import cythonize
+#from distutils.extension import Extension
 import numpy as np
 
 # To install and compile to your anaconda/python site-packages, simply run:
@@ -12,15 +13,19 @@ ext_modules = [
     Extension(
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=[] # originally was ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        include_dirs=[np.get_include(), '../common'],
+        extra_compile_args=[]  # originally was ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
 setup(name='pycocotools',
       packages=['pycocotools'],
-      package_dir = {'pycocotools': 'pycocotools'},
+      package_dir={'pycocotools': 'pycocotools'},
       version='2.0',
-      ext_modules=
-          cythonize(ext_modules)
+      ext_modules=ext_modules,
+      install_requires=[
+          'setuptools',
+          'cython',
+          'numpy',
+          'matplotlib']
       )
